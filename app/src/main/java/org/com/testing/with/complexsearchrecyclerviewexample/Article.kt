f192db78cdfd1c95ed92364d06b17bccf6c26414
joNA <jitzian@gmail.com>
2021-04-03 11:46:47 -0400

    Initial commit

---
+++ app/src/main/java/org/com/testing/with/complexsearchrecyclerviewexample/Article.kt
@@ -0,0 +1,3 @@
+package org.com.testing.with.complexsearchrecyclerviewexample
+
+data class Article(var name: String? = null, var content: String? = null)
---
+++ app/src/main/java/org/com/testing/with/complexsearchrecyclerviewexample/RVCustomAdapter.kt
@@ -0,0 +1,16 @@
+package org.com.testing.with.complexsearchrecyclerviewexample
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RVCustomAdapter {
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        fun bindDta(){
+
+        }
+
+    }
+
+}
