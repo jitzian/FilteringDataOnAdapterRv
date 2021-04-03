4b284f27791d2e0439fe6ec1d9bfd3b30bedb0d0
joNA <jitzian@gmail.com>
2021-04-03 15:13:54 -0400

    Making filter to work with Article data class but, I don't like the implementation. I will work on refactoring it

--- app/src/main/java/org/com/testing/with/complexsearchrecyclerviewexample/MainActivity.kt
+++ app/src/main/java/org/com/testing/with/complexsearchrecyclerviewexample/MainActivity.kt
@@ -2,6 +2,7 @@ package org.com.testing.with.complexsearchrecyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
 import org.com.testing.with.complexsearchrecyclerviewexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,34 +16,50 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initSearchView()
         initRV()
     }
 
     private fun initRV() {
-        adapter = RVCustomAdapter(getData())
+        adapter = RVCustomAdapter(getData() as ArrayList<Article>)
         binding.mRecyclerViewMainArticles.adapter = adapter
+    }
+
+    private fun initSearchView() {
+        binding.mSearchViewMainActivity.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                return false
+            }
+
+        })
     }
 
     private fun getData() =
         mutableListOf(
             Article(
-                "Important Title 1",
+                "Darinka",
                 "The PNG format is widely supported and works best with presentations and web design. "
             ),
             Article(
-                "Important Title 2",
+                "Ana Darinka Fernandez Mosqueda",
                 "The PNG format is widely supported and works best with presentations and web design. "
             ),
             Article(
-                "Important Title 3",
+                "Ulises Fernandez Mosqueda",
                 "The PNG format is widely supported and works best with presentations and web design. "
             ),
             Article(
-                "Important Title 4",
+                "Hector Lucio Mosqueda",
                 "The PNG format is widely supported and works best with presentations and web design. "
             ),
             Article(
-                "Important Title 5",
+                "Victoria Nicole Lucio Mosqueda",
                 "The PNG format is widely supported and works best with presentations and web design. "
             )
         )
--- app/src/main/java/org/com/testing/with/complexsearchrecyclerviewexample/RVCustomAdapter.kt
+++ app/src/main/java/org/com/testing/with/complexsearchrecyclerviewexample/RVCustomAdapter.kt
@@ -1,47 +1,85 @@
 package org.com.testing.with.complexsearchrecyclerviewexample
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import java.util.*
+import kotlin.collections.ArrayList
 
-class RVCustomAdapter(var lstRes: List<Article>?) :
-    RecyclerView.Adapter<RVCustomAdapter.ViewHolder>() {
+class RVCustomAdapter(private var lstRes: ArrayList<Article>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>(), Filterable {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.card_view_item, parent, false)
-        )
+    var lstResFiltered = ArrayList<Article>()
+
+    lateinit var mcontext: Context
+
+    class CountryHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    init {
+        lstResFiltered = lstRes
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        lstRes?.get(position)?.let { holder.bindDta(it) }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val countryListView =
+            LayoutInflater.from(parent.context).inflate(R.layout.card_view_item, parent, false)
+        val sch = CountryHolder(countryListView)
+        mcontext = parent.context
+        return sch
     }
 
     override fun getItemCount(): Int {
-        return lstRes?.size ?: 0
+        return lstResFiltered.size
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
-        private val mImageViewCardViewItem: ImageView =
-            itemView.findViewById(R.id.mImageViewCardViewItem)
-        private val mTextViewTitle: TextView = itemView.findViewById(R.id.mTextViewTitle)
-        private val mTextViewContent: TextView = itemView.findViewById(R.id.mTextViewContent)
+        Picasso.get()
+            .load("https://cdn4.iconfinder.com/data/icons/bettericons/354/github-512.png")
+            .into(holder.itemView.findViewById<ImageView>(R.id.mImageViewCardViewItem))
+        holder.itemView.findViewById<TextView>(R.id.mTextViewTitle).text =
+            lstResFiltered[position].title
+        holder.itemView.findViewById<TextView>(R.id.mTextViewContent).text =
+            lstResFiltered[position].content
 
-        fun bindDta(data: Article) {
-            Picasso.get()
-                .load("https://cdn4.iconfinder.com/data/icons/bettericons/354/github-512.png")
-                .into(mImageViewCardViewItem)
+    }
 
-            mTextViewTitle.text = data.title
-            mTextViewContent.text = data.content
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                lstResFiltered = if (charSearch.isEmpty()) {
+                    lstRes
+                } else {
+                    val resultList = ArrayList<Article>()
+                    for (row in lstRes) {
+                        if (row.title?.toLowerCase(Locale.ROOT)
+                                ?.contains(charSearch.toLowerCase(Locale.ROOT)) == true
+                        ) {
+                            resultList.add(row)
+                        }
+                    }
+                    resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = lstResFiltered
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                lstResFiltered = results?.values as ArrayList<Article>
+                notifyDataSetChanged()
+            }
+
         }
-
     }
-
 
 }
